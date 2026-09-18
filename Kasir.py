e2f371fcb294397b454b7e5c17c37dc969d1576a
Nadhifa Aprilia <ndhfaaprilia@apps.ipb.ac.id>
2026-09-18 11:56:35 +0700

    Membuat program awal kasir

---
+++ Kasir.py
@@ -0,0 +1,10 @@
+print("=== PROGRAM KASIR SEDERHANA ===")
+
+nama_barang = input("Nama barang: ")
+harga = int(input("Harga barang: "))
+jumlah = int(input("Jumlah barang: "))
+
+print("\nData barang berhasil dicatat.")
+print("Nama barang:", nama_barang)
+print("Harga:", harga)
+print("Jumlah:", jumlah)
