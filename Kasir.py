e9d551e9e7d17e6231f1323091e5cd179db539cd
Anam <ayubianam2@gmail.com>
2026-09-18 07:26:23 +0000

    Menambahkan fitur perhitungan total

--- Kasir.py
+++ Kasir.py
@@ -1,10 +1,16 @@
+
 print("=== PROGRAM KASIR SEDERHANA ===")
 
 nama_barang = input("Nama barang: ")
 harga = int(input("Harga barang: "))
 jumlah = int(input("Jumlah barang: "))
 
+total = harga * jumlah
+
 print("\nData barang berhasil dicatat.")
 print("Nama barang:", nama_barang)
 print("Harga:", harga)
 print("Jumlah:", jumlah)
+
+print("\n=== TOTAL BELANJA ===")
+print("Total belanja:", total)
