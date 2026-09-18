f1453a20553effaff2b5b788aea2cd842a645d93
Nadhifa Aprilia <ndhfaaprilia@apps.ipb.ac.id>
2026-09-18 12:32:27 +0700

    Menambahkan fitur input beberapa barang

--- Kasir.py
+++ Kasir.py
@@ -1,10 +1,25 @@
 print("=== PROGRAM KASIR SEDERHANA ===")
 
-nama_barang = input("Nama barang: ")
-harga = int(input("Harga barang: "))
-jumlah = int(input("Jumlah barang: "))
+jumlah_jenis = int(input("Berapa jenis barang yang dibeli: "))
 
-print("\nData barang berhasil dicatat.")
-print("Nama barang:", nama_barang)
-print("Harga:", harga)
-print("Jumlah:", jumlah)
+data_barang = []
+
+for i in range(jumlah_jenis):
+    print(f"\n--- Barang ke-{i+1} ---")
+
+    nama_barang = input("Nama barang: ")
+    harga = int(input("Harga satuan: "))
+    jumlah_beli = int(input("Jumlah beli: "))
+
+    data_barang.append({
+        "nama": nama_barang,
+        "harga": harga,
+        "jumlah": jumlah_beli
+    })
+
+print("\n=== DATA BARANG ===")
+
+for barang in data_barang:
+    print("Nama barang:", barang["nama"])
+    print("Harga satuan:", barang["harga"])
+    print("Jumlah beli:", barang["jumlah"])
